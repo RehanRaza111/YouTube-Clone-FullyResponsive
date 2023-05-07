c8d5a72c843f0be2143c0c2a3229117b793e7607
RehanRaza111 <Rehan.jafferi110@gmail.com>
2023-05-07 17:04:36 +0500

    New-Project-Added
    Designed fully responsive YouTube Clone App Using HTML, CSS & JS.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const menuIcon = document.querySelector('.menu-icon');
+const sideBar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+
+menuIcon.onclick = function () {
+    
+    sideBar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container');
+}
+
