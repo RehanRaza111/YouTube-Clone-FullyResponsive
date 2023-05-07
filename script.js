19cb9ddc81cfc9dd1ac27169acb454f12b7aeae5
RehanRaza111 <Rehan.jafferi110@gmail.com>
2023-05-07 19:11:58 +0500

    styling updated

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const menuIcon = document.querySelector('.menu-icon');
 const sideBar = document.querySelector('.sidebar');
 const container = document.querySelector('.container');
+const largeFooter =document.querySelector('.footer')
 
 menuIcon.onclick = function () {
     
     sideBar.classList.toggle('small-sidebar');
     container.classList.toggle('large-container');
+    largeFooter.classList.toggle('large-footer');
 }
 
